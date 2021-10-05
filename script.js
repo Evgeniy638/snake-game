c91374e5763708d107a24e1de4055b07d66b0470
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-10-05 11:07:49 +0300

    add draw and fillField

--- script.js
+++ script.js
@@ -1,3 +1,98 @@
-function pow(x, n) {
-    return Math.pow(x, n);
+const field = document.querySelector('.field');
+
+const typeCell = {
+    APPLE: "APPLE",
+    SNAKE: "SNAKE",
+    EMPTY: "EMPTY",
+};
+
+const typesDirection = {
+    RIGHT: "RIGHT",
+    LEFT: "LEFT",
+    DOWN: "DOWN",
+    UP: "UP",
+};
+
+const size = {
+    width: 30,
+    heith: 30
+};
+
+let snake = [];
+let matrixCell = [];
+let matrix = [];
+let direction = typesDirection.LEFT;
+
+window.onload = () => {
+    fillField(size.width, size.heith);
+    document.addEventListener("click", handleClick);
+    gameLoop();
+};
+
+function fillField(width, height) {
+    for(let y = 0; y < height; y++) {
+        matrix.push([]);
+        matrixCell.push([]);
+
+        for(let x = 0; x < width; x++) {
+            const cell = document.createElement('div');
+            cell.classList.add("cell");
+
+            matrix[y].push(typeCell.EMPTY);
+            matrixCell[y].push(cell);
+            field.appendChild(cell);
+        }
+    }
+
+    snake[0] = {
+        x: Math.round(width / 2),
+        y: Math.round(height / 2),
+    };
+
+    snake[1] = {
+        x: snake[0].x + 1,
+        y: snake[0].y,
+    };
+
+    matrix[snake[0].y][snake[0].x] = typeCell.SNAKE;
+    matrix[snake[1].y][snake[1].x] = typeCell.SNAKE;
+
+    matrix = randomApple(matrix, snake);
+    draw(matrix);
+}
+
+function gameLoop() {
+
+}
+
+function draw(matrix) {
+    matrix.forEach((row, y) => {
+        row.forEach((cell, x) => {
+            matrixCell[y][x].classList.remove('apple');
+            matrixCell[y][x].classList.remove('snake');
+
+            switch (cell) {
+                case typeCell.APPLE:
+                    matrixCell[y][x].classList.add('apple');
+                    break;
+                case typeCell.SNAKE:
+                    matrixCell[y][x].classList.add('snake');
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+}
+
+function move(matrix, snake, direction) {
+    return matrix;
+}
+
+function handleClick(e) {
+
+}
+
+function randomApple(matrix, snake) {
+    return matrix;
 }
